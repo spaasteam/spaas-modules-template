9a46c4a4e46d4bc554f83a98d69bc094db1c98a7
jack-rainbow <987709173@qq.com>
2020-03-17 17:36:33 +0800

    fix: 修改组件为ts模板

--- main/components/NoPermission/index.vue
+++ main/components/NoPermission/index.vue
@@ -1,33 +1,33 @@
-<template>
-  <div class="no-permission-component">
-    <img :src="noLimit" alt="您暂时没有查看的权限" class="img-block" />
-    <p class="sorry-info">抱歉，您暂时没有查看的权限</p>
-    <el-button type="primary" @click="goBackToIndex">返回首页</el-button>
-  </div>
-</template>
-
-<script>
+<script lang="tsx">
+import {Vue, Component, Prop, Emit} from 'vue-property-decorator';
 import noLimit from './images/noLimit.png';
 
-export default {
-  props: {
-    goIndex: Function,
-  },
-  data() {
-    return {
-      noLimit,
-    };
-  },
-  methods: {
-    goBackToIndex() {
-      if (this.goIndex) {
-        this.goIndex();
-      } else {
-        this.$router.replace('/');
-      }
-    },
-  },
-};
+@Component({})
+export default class NoPermiss extends Vue {
+  private noLimit;
+
+  @Prop(Function) readonly goIndex;
+
+  @Emit()
+  goBackToIndex() {
+    if (this.goIndex) {
+      this.goIndex();
+    } else {
+      this.$router.replace('/');
+    }
+  }
+  render() {
+    return (
+      <div class="no-permission-component">
+        <img src={noLimit} alt="您暂时没有查看的权限" class="img-block" />
+        <p class="sorry-info">抱歉，您暂时没有查看的权限</p>
+        <el-button type="primary" onclick={this.goBackToIndex}>
+          返回首页
+        </el-button>
+      </div>
+    );
+  }
+}
 </script>
 <style lang="less">
 .no-permission-component {
